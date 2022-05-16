36fbb174dcb9315f4a66cd6557a1b0e8b8ee7626
LeonCookie <leongdynia1105@gmail.com>
2022-05-16 20:02:48 +0200

    change read file into string and accept space;

--- AlgorytymyPrzeszukiwania/MainWindow.xaml.cs
+++ AlgorytymyPrzeszukiwania/MainWindow.xaml.cs
@@ -29,19 +29,19 @@ namespace AlgorytymyPrzeszukiwania
             InitializeComponent();
         }
 
-        //var
-        List<String> Lista = new List<String>(); // our list for string
+
+        int Hmany = 0;
         int Irepeat = 1;  //repeat count
         string SSsearch;// searching value
-        
-        string[] sTable;
+        string dane;// main string
 
 
 
 
         private void Btn_load_Click(object sender, RoutedEventArgs e)
         {
-            Lista.Clear();
+            dane = string.Empty;
+            Debug.WriteLine("saassadddddddddddddsad plus " + dane);
             stringholder.ItemsSource = "";//
             TabControl_Main.IsEnabled = true;
 
@@ -50,21 +50,16 @@ namespace AlgorytymyPrzeszukiwania
             {
                 using (var streamReader = File.OpenText(openFileDialog.FileName))// czytanie z pliku
                 {
-                    var s = string.Empty;
-                    while ((s = streamReader.ReadLine()) != null)
-                        Lista.Add(s);  
+                    dane = System.IO.File.ReadAllText(openFileDialog.FileName);
                 }
-                stringholder.ItemsSource = Lista;
-                Lista.ForEach(x => Debug.WriteLine(x));// write Lista in Debug window for check
-                sTable = Lista.ToArray();// change list to array
-                Debug.WriteLine("===========================");
-                for (int i = 0; i < sTable.Length; i++)
-                {
-                    Debug.WriteLine(sTable[i]);
-                }// write sTable in Debug window
+                stringholder.ItemsSource = dane;
+                
 
 
             }
+            Debug.WriteLine(dane);
+            MessageBox.Show("Długość twojego napisu to:" + dane.Length+"To twoj napis:" + dane);
+          
             
         }
 
@@ -77,7 +72,6 @@ namespace AlgorytymyPrzeszukiwania
                 e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);// tylko liczby w NumberInput-  ilosc powtorzen
             }
             Irepeat = int.Parse(repeatInput.Text);
-            Debug.WriteLine(Irepeat);
             repeatInput.Text = "";
             repeatInput.Text = "" + Irepeat;
             MessageBox.Show("twoja liczba powtorzen: " + Irepeat);
@@ -95,21 +89,36 @@ namespace AlgorytymyPrzeszukiwania
             {
                 Stopwatch stopwatch = new Stopwatch();//timer
                 SSsearch = SearchInput.Text;
-                stringholder.ItemsSource = Lista;
-                Debug.WriteLine("LIsta:");
+            
 
                 stopwatch.Start();
                 //sorting(brutalforce)
+
+
+                Hmany = 0;
+
+                for (int k = 1; k<  Irepeat ; k++)
+                {
+                        for(int i=0; i < dane.Length; i++)
+                        {
+                            for (int j = 0; j < SSsearch.Length; j++)
+                            {
+                            if (dane[i + j] != SSsearch[j])
+                                {
+                                Hmany += 1;
+                                }
+                            }
+                        }
+                }
                 
 
-                for (int i = 1; Irepeat > 0; i++)
-                {
 
-                }
 
 
 
                 stopwatch.Stop();
+                
+                MessageBox.Show("znaleznion" + Hmany);
 
 
                 TEXT_time_BF.Text = "" + stopwatch.ElapsedMilliseconds;
@@ -130,32 +139,23 @@ namespace AlgorytymyPrzeszukiwania
             else
             {
                 SSsearch = SearchInput.Text;
-                stringholder.ItemsSource = Lista;
+             
 
-                string lancuch = String.Join("", Lista.ToArray());
-                Debug.WriteLine(lancuch);
+             
 
                
-                int Nsearch = SSsearch.Length; // lenght of searched
-                int Mlist = sTable.Length; //lenght of table
-
-                int Mlista1 = Mlist + 1;
+                
 
 
                 Stopwatch stopwatch = new Stopwatch();//timer
 
                 stopwatch.Start();
 
-                Debug.WriteLine(Nsearch);
-                Debug.WriteLine(Mlist);
 
-
-
-                for (int i = 1; Irepeat > 0; i++)
+                for (int k = 1; k < Irepeat; k++)
                 {
 
                 }
-
 
 
 
@@ -183,10 +183,27 @@ namespace AlgorytymyPrzeszukiwania
             }
             else
             {
-                for (int i = 1; Irepeat > 0; i++)
+                Stopwatch stopwatch = new Stopwatch();//timer
+
+                stopwatch.Start();
+
+
+                for (int k = 1; k < Irepeat; k++)
                 {
 
                 }
+
+
+
+
+
+
+
+
+                stopwatch.Stop();
+
+
+                TEXT_time_BM.Text = "" + stopwatch.ElapsedMilliseconds;
             }
             
 
@@ -207,59 +224,21 @@ namespace AlgorytymyPrzeszukiwania
             {
 
 
-                int[] SearchString(string A, string B)
-                {
-                    sum = 0;
-                    List<int> retVal = new List<int>();
-                    ulong siga = 0;
-                    ulong sigb = 0;
-                    ulong Q = 100007;
-                    ulong D = 256;
-
-                    for (int i = 0; i < B.Length; ++i)
-                    {
-                        siga = (siga * D + (ulong)A[i]) % Q;
-                        sigb = (sigb * D + (ulong)B[i]) % Q;
-                    }
-
-                    if (siga == sigb)
-                        retVal.Add(0);
-
-                    ulong pow = 1;
-
-                    for (int k = 1; k <= B.Length - 1; ++k)
-                        pow = (pow * D) % Q;
-
-                    for (int j = 1; j <= A.Length - B.Length; ++j)
-                    {
-                        siga = (siga + Q - pow * (ulong)A[j - 1] % Q) % Q;
-                        siga = (siga * D + (ulong)A[j + B.Length - 1]) % Q;
-
-                        if (siga == sigb)
-                            if (A.Substring(j, B.Length) == B)
-                                retVal.Add(j);
-                        sum += 1;
-
-                    }
-
-                    Debug.WriteLine(retVal);
-                    return retVal.ToArray();
-                }
+               
+                
 
                 Stopwatch stopwatch = new Stopwatch();//timer
 
-                string lancuch = String.Join("", Lista.ToArray());
+               
 
                 SSsearch = SearchInput_RK.Text;
                 stopwatch.Start();
 
-
-                for (int i = 1; Irepeat > 0; i++)
+                for (int k = 1; k < Irepeat; k++)
                 {
 
-                    int[] value = SearchString(lancuch, SSsearch);
-
                 }
+
                 stopwatch.Stop();
 
                 TEXT_time_RK.Text = "" + stopwatch.ElapsedMilliseconds;
